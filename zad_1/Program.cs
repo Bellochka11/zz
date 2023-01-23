992ac82edbf7da22bef3da5854a07c0da0119649
Белянкина Елизавета <Belyankina@mail.ru>
2023-01-23 15:40:11 +0300

    первая задача

---
+++ zad_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+int num = new Random().Next(10, 100);
+int num1 = num / 10;
+int num2 = num % 10;
+int max = num1;
+if (num2 > max)
+{
+    max = num2;
+}
+
+Console.WriteLine($"Максимальное число {num} -> {max}");
